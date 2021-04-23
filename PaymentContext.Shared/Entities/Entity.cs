f44805473579bdd6b49126c6c7dd091e81dff77b
hebrain <hebrasantos@gmail.com>
2021-04-23 19:36:57 -0300

    Create payment service

---
+++ PaymentContext.Shared/Entities/Entity.cs
@@ -0,0 +1,17 @@
+﻿using Flunt.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentContext.Shared.Entities
+{
+    public abstract class Entity : Notifiable
+    {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; private set; }
+    }
+}
---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests.Entities
+{
+    [TestClass]
+    public class StudentTests
+    {
+        public void AdicionarAssinatura()
+        {
+            var subscription = new Subscription(null);
+            var student = new Student("Hebra", "Tabosa", "123456789", "hello@hebra.com");
+            student.AddSubscription(subscription);
+        }
+    }
+}
